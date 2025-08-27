2ed0f85dec4cccb82a32c3918b2fb78acb6c5550
Jasonxia007 <iamjasonxia@126.com>
2025-08-27 18:52:31 +0800

    🐛 Fix init.sql missing partner_mapping_id_t

--- docker/sql/0820_add_partner_mapping_id_t.sql
+++ docker/sql/0820_add_partner_mapping_id_t.sql
@@ -5,7 +5,7 @@ MAXVALUE 2147483647
 START 1
 CACHE 1;
 
-CREATE TABLE "nexent"."partner_mapping_id_t" (
+CREATE TABLE IF NOT EXISTS "nexent"."partner_mapping_id_t" (
   "mapping_id" serial PRIMARY KEY NOT NULL,
   "external_id" varchar(100) COLLATE "pg_catalog"."default",
   "internal_id" int4,
@@ -16,53 +16,32 @@ CREATE TABLE "nexent"."partner_mapping_id_t" (
   "update_time" timestamp(6) DEFAULT CURRENT_TIMESTAMP,
   "created_by" varchar(100) COLLATE "pg_catalog"."default",
   "updated_by" varchar(100) COLLATE "pg_catalog"."default",
-  "delete_flag" varchar(1) COLLATE "pg_catalog"."default" DEFAULT 'N'::character varying,
-)
-;
+  "delete_flag" varchar(1) COLLATE "pg_catalog"."default" DEFAULT 'N'::character varying
+);
 
 ALTER TABLE "nexent"."partner_mapping_id_t" OWNER TO "root";
 
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."mapping_id" IS 'ID';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."external_id" IS 'The external id given by the outer partner';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."internal_id" IS 'The internal id of the other database table';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."mapping_type" IS 'Type of the external - internal mapping, value set: CONVERSATION';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."tenant_id" IS 'Tenant ID';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."user_id" IS 'User ID';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."create_time" IS 'Creation time';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."update_time" IS 'Update time';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."created_by" IS 'Creator';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."updated_by" IS 'Updater';
-
 COMMENT ON COLUMN "nexent"."partner_mapping_id_t"."delete_flag" IS 'Whether it is deleted. Optional values: Y/N';
 
-SELECT setval('"nexent"."partner_mapping_id_t_mapping_id_seq"', 1, false);
-
-ALTER SEQUENCE "nexent"."partner_mapping_id_t_mapping_id_seq"
-OWNED BY "nexent"."partner_mapping_id_t"."mapping_id";
-
-ALTER SEQUENCE "nexent"."partner_mapping_id_t_mapping_id_seq" OWNER TO "root";
-
-CREATE FUNCTION "nexent"."update_partner_mapping_update_time"()
-  RETURNS "pg_catalog"."trigger" AS $BODY$
+CREATE OR REPLACE FUNCTION "update_partner_mapping_update_time"()
+RETURNS TRIGGER AS $$
 BEGIN
     NEW.update_time = CURRENT_TIMESTAMP;
     RETURN NEW;
 END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
+$$ LANGUAGE plpgsql;
 
-ALTER FUNCTION "nexent"."update_partner_mapping_update_time"() OWNER TO "root";
-
-CREATE TRIGGER "update_partner_mapping_update_time_trigger" BEFORE UPDATE ON "nexent"."partner_mapping_id_t"
+CREATE TRIGGER "update_partner_mapping_update_time_trigger"
+BEFORE UPDATE ON "nexent"."partner_mapping_id_t"
 FOR EACH ROW
-EXECUTE PROCEDURE "nexent"."update_partner_mapping_update_time"();
+EXECUTE FUNCTION "update_partner_mapping_update_time"();
